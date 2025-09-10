215057d7afb60c5cf1258a5b1442f30d74a061cd
Marcus Plech <marcusplech@MacBook-Air-de-Marcus.local>
2025-09-10 16:06:50 -0300

    Remove duplicate files

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mini-seller-console/',
+  base: "/",
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
